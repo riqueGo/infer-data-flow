7185dd5d6b1965420aa8cee5af479b24d904fdc4
henrique.oliveira.gomes@gmail.com <henrique.oliveira.gomes@gmail.com>
2024-12-10 23:14:31 -0300

    PrefixExpression and PostfixExpression visitor was created

---
+++ src/main/java/org/example/infer/InferUtils.java
@@ -0,0 +1,6 @@
+package org.example.infer;
+
+public class InferUtils {
+    public static final String packageName = "inferDependencies";
+    public static final String wrapperClassName = "InferWrapper";
+}
