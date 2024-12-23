f993dbf7945341ffe813a316558faa79eb979f51
henrique.oliveira.gomes@gmail.com <henrique.oliveira.gomes@gmail.com>
2024-12-23 18:57:24 -0300

    Infer Analysis updated

--- src/main/java/infer/InferAnalysis.java
+++ src/main/java/infer/InferAnalysis.java
@@ -4,9 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
-import static infer.InferUtils.INFER_PACKAGE_PATH;
+import static infer.InferUtils.*;
 
 public class InferAnalysis {
     private InferConfig inferConfig;
@@ -19,34 +21,86 @@ public class InferAnalysis {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(inferConfig);
 
-        try (FileWriter writer = new FileWriter(Path.of(projectPath, INFER_PACKAGE_PATH, "inferConfig.json").toString())) {
+        Path inferPackagePath = Path.of(projectPath, INFER_PACKAGE_PATH);
+        String inferConfigUri = inferPackagePath.resolve(INFER_CONFIG_NAME).toString();
+        String inferOutUri = inferPackagePath.resolve(INFER_OUT).toString();
+        String gradlewPath = Path.of(projectPath, "gradlew").toString();
+
+        String buildCommand;
+        if (isGradleProject(projectPath)) {
+            buildCommand = String.format("%s clean build", gradlewPath);
+        } else if (isMavenProject(projectPath)) {
+            buildCommand = String.format("mvn clean install");
+        } else {
+            throw new IllegalStateException("Unsupported project type. Neither Gradle nor Maven build files were found.");
+        }
+
+        try (FileWriter writer = new FileWriter(inferConfigUri)) {
             writer.write(json);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //TODO: Avoid Hardcode
-        System.out.println("Infer Executing left -> right...");
-        execInfer("infer --pulse-only --pulse-taint-config /home/rique/Documents/research/infer-data-flow-test/src/main/java/infer/inferConfig.json -o /home/rique/Documents/research/infer-data-flow-test/src/main/java/infer/infer-out-left-to-right -- /home/rique/Documents/research/infer-data-flow-test/gradlew clean build");
-        inferConfig.swap();
+        String captureCommand = String.format("infer capture -o %s -- %s", inferOutUri, buildCommand);
+        String analyzeCommand = String.format("infer analyze --pulse-only --pulse-taint-config %s -o %s", inferConfigUri, inferOutUri);
 
+        System.out.println(captureCommand);
+        System.out.println("Capture Infer Phase...");
+        execInfer(captureCommand, projectPath);
+
+        System.out.println("Infer Executing left -> right");
+        System.out.println(analyzeCommand);
+        execInfer(analyzeCommand, projectPath);
+
+        renameReportFile(inferOutUri, "left-to-right-report.txt");
+
+        inferConfig.swap();
         json = gson.toJson(inferConfig);
 
-        try (FileWriter writer = new FileWriter(Path.of(projectPath, INFER_PACKAGE_PATH, "inferConfig.json").toString())) {
+        try (FileWriter writer = new FileWriter(Path.of(projectPath, INFER_PACKAGE_PATH, INFER_CONFIG_NAME).toString())) {
             writer.write(json);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //TODO: Avoid Hardcode
         System.out.println("Infer Executing right -> left...");
-        execInfer("infer --pulse-only --pulse-taint-config /home/rique/Documents/research/infer-data-flow-test/src/main/java/infer/inferConfig.json -o /home/rique/Documents/research/infer-data-flow-test/src/main/java/infer/infer-out-right-to-left -- /home/rique/Documents/research/infer-data-flow-test/gradlew clean build");
+        System.out.println(analyzeCommand);
+        execInfer(analyzeCommand, projectPath);
+
+        renameReportFile(inferOutUri, "right-to-left-report.txt");
     }
 
-    private void execInfer(String command) {
+    private void renameReportFile(String inferOutUri, String newFileName) {
+        Path reportFile = Path.of(inferOutUri, "report.txt");
+        Path renamedReportFile = Path.of(inferOutUri, newFileName);
+
+        try {
+            if (Files.exists(reportFile)) {
+                Files.move(reportFile, renamedReportFile, StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (IOException e) {
+            System.err.println("Error renaming report file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isGradleProject(String projectPath) {
+        Path gradleFile = Path.of(projectPath, "build.gradle");
+        Path gradleKtsFile = Path.of(projectPath, "build.gradle.kts");
+        Path gradlewFile = Path.of(projectPath, "gradlew");
+        return Files.exists(gradleFile) || Files.exists(gradleKtsFile) || Files.exists(gradlewFile);
+    }
+
+    private boolean isMavenProject(String projectPath) {
+        Path mavenFile = Path.of(projectPath, "pom.xml");
+        return Files.exists(mavenFile);
+    }
+
+
+    private void execInfer(String command, String pathExecution) {
 
         ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
-        processBuilder.directory(new File("/home/rique/Documents/research/infer-data-flow-test"));
+        processBuilder.directory(new File(pathExecution));
 
         try {
             Process process = processBuilder.start();
--- src/main/java/infer/InferUtils.java
+++ src/main/java/infer/InferUtils.java
@@ -7,4 +7,6 @@ public class InferUtils {
     public static final String WRAPPER_CLASS_NAME = "InferWrapper";
     public static final String SOURCE_PROJECT_PATH = "/src/main/java"; //TODO: This should be passed as program argument
     public static final String INFER_PACKAGE_PATH = Path.of(SOURCE_PROJECT_PATH, INFER_PACKAGE_NAME).toString();
+    public static final String INFER_CONFIG_NAME = "inferconfig.json";
+    public static final String INFER_OUT = "infer-out";
 }
